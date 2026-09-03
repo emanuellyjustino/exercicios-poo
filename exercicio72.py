50d544211e80ecf3e18418be1a05aed5b727e7ff
emanuelly <emanuelly.justino@escolar.ifrn.edu.br>
2026-09-03 20:21:09 -0300

    Add files via upload

---
+++ exercicio72.py
@@ -0,0 +1,6 @@
+contagem = (
+    'zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+    'seis', 'sete', 'oito', 'nove', 'dez',
+    'onze', 'doze', 'treze', 'quatorze', 'quinze',
+    'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+)
