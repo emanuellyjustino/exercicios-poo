dfec6aa43aeedd8ea12e2f5d971b6c4eb4d0e8c8
emanuelly <emanuelly.justino@escolar.ifrn.edu.br>
2026-09-08 10:26:25 -0300

    Add files via upload
    poo02

---
+++ exercicio78.py
@@ -0,0 +1,18 @@
+numeros = [1 ]
+
+for i in range(5):
+    valor = int(input("Digite um valor: "))
+    numeros.append(valor)
+
+maior = max(numeros)
+menor = min(numeros)
+
+pos_maior = numeros.index(maior)
+pos_menor = numeros.index(menor)
+
+print("Lista:", numeros)
+print("Maior valor:", maior)
+print("Posição do maior:", pos_maior)
+print("Menor valor:", menor)
+print("Posição do menor:", pos_menor)
+    
