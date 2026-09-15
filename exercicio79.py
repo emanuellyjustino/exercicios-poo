b4a6092d6e7b8538f709265dee12752e508b3691
emanuelly <emanuelly.justino@escolar.ifrn.edu.br>
2026-09-15 09:12:45 -0300

    Refactor to input numbers and find unique values

--- exercicio79.py
+++ exercicio79.py
@@ -1,4 +1,10 @@
-valores = [5, 2, 5, 8, 3]
+valores = []
+
+quantidade = int(input("Quantos números você quer digitar? "))
+
+for i in range(quantidade):
+    numero = int(input("Digite um número: "))
+    valores.append(numero)
 
 lista = []
 
