a66cf5ef2be236c17440ea7c5070ba98b89774f6
emanuelly <emanuelly.justino@escolar.ifrn.edu.br>
2026-09-15 09:14:19 -0300

    Refactor to allow user input for number list

--- exercicio81.py
+++ exercicio81.py
@@ -1,4 +1,10 @@
-valores = [5, 2, 8, 1, 5, 10]
+valores = []
+
+quantidade = int(input("Quantos números você quer digitar? "))
+
+for i in range(quantidade):
+    numero = int(input("Digite um número: "))
+    valores.append(numero)
 
 print("Quantidade de números digitados:", len(valores))
 
